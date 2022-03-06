4461435401d8d66d724a6739ea441881acc480c0
Guissmo Asuncion <guissmo@gmail.com>
2022-03-06 23:08:26 +0100

    can now console.log primalitycertificate!

--- app.js
+++ app.js
@@ -7,32 +7,25 @@ const hb = require('handlebars');
 
 const server = http.createServer(app);
 
-const {exec} = require('child_process');
+const pari = require('./util/pari')
 
-const latex = require('node-latex');
-const input = fs.readFileSync('./aux_files/test-template.tex');
+// const latex = require('node-latex');
+// const input = fs.readFileSync('./aux_files/test-template.tex');
 
-console.log(input)
+// console.log(input)
 
-const temp = hb.compile(input.toString());
-const contents = temp({number: '8675309'});
+// const temp = hb.compile(input.toString());
+// const contents = temp({number: '8675309'});
 
-console.log(contents)
+// console.log(contents)
 
-const output = fs.createWriteStream('./aux_files/output.pdf');
-const pdf = latex(contents)
-pdf.pipe(output)
-pdf.on('error', err => console.error(err))
-pdf.on('finish', () => console.log('PDF generated!'))
+// const output = fs.createWriteStream('./aux_files/output.pdf');
+// const pdf = latex(contents)
+// pdf.pipe(output)
+// pdf.on('error', err => console.error(err))
+// pdf.on('finish', () => console.log('PDF generated!'))
 
-const prime = '100000000000000000000000000000000069';
-fs.writeFile('./aux_files/script.gp', 'write("./aux_files/cert.txt",primecert('+prime+'))', err => {
-    if(err) {
-        console.log(err)
-    }
-    return
-})
-exec('gp ./aux_files/script.gp');
+pari.primalityCertificate("10^35+69")
 
 app.use('/', (req, res, next) => {
     res.send("Hello world...")
