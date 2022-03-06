59701ba07baf1d875feab2868da7659d1f5cdc52
Guissmo Asuncion <guissmo@gmail.com>
2022-03-06 16:33:57 +0100

    exec Hello World

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const http = require('http')
 const express = require('express')
-
 const app = express();
 
 const server = http.createServer(app);
 
+const {exec} = require('child_process')
+const child = exec('echo Hello World')
+
+child.stdout.on('data', (msg) => {
+    console.log(`I said: ${msg}`);
+})
+
 app.use('/', (req, res, next) => {
-    res.send('<h1>Hello World!</h1>')
+    res.send("Hello world...")
 })
 
 server.listen(3000)
