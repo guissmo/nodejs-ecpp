401fc9c3012a4302b152613d17d3d17cd6591218
Guissmo Asuncion <guissmo@gmail.com>
2022-03-06 16:58:20 +0100

    can now compile static tex file

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const http = require('http')
 const express = require('express')
 const app = express();
 
+const fs = require('fs');
+
 const server = http.createServer(app);
 
-const {exec} = require('child_process')
-const child = exec('echo Hello World')
+const {exec} = require('child_process');
 
-child.stdout.on('data', (msg) => {
-    console.log(`I said: ${msg}`);
+const latex = require('node-latex');
+const input = fs.createReadStream('./aux_files/test.tex');
+const output = fs.createWriteStream('./aux_files/output.pdf');
+const pdf = latex(input)
+pdf.pipe(output)
+pdf.on('error', err => console.error(err))
+pdf.on('finish', () => console.log('PDF generated!'))
+
+const prime = '100000000000000000000000000000000069';
+fs.writeFile('./aux_files/script.gp', 'write("./aux_files/cert.txt",primecert('+prime+'))', err => {
+    if(err) {
+        console.log(err)
+    }
+    return
 })
+exec('gp ./aux_files/script.gp');
 
 app.use('/', (req, res, next) => {
     res.send("Hello world...")
