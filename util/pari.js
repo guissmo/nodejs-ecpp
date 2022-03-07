fd5f10c3e635a57f52d54bc42b2387a5afad564a
Guissmo Asuncion <guissmo@gmail.com>
2022-03-07 16:14:36 +0100

    fixed pari script writer

--- util/pari.js
+++ util/pari.js
@@ -15,7 +15,7 @@ const fsReadFileSync = util.promisify(fs.readFileSync);
 async function primalityCertificate(integer) {
     await fs.writeFileSync(
         './aux_files/script.gp',
-        'F=fileopen("./aux_files/cert.txt","w");N=primecert('+integer+');filewrite(F,vector(#N,i,vector(#N[i],j,Str(N[i][j]))));fileclose(F);quit()',
+        'F=fileopen("./aux_files/cert.txt","w");N=primecert('+integer+');filewrite(F, vector(#N,i, apply( x->Str(x), [i, N[i][1],N[i][2], N[i][1]+1-N[i][2], (N[i][1]+1-N[i][2])/N[i][3], N[i][4], (N[i][5][2]^2-N[i][5][1]^3-N[i][4]*N[i][5][1])%N[i][1], N[i][5][1], N[i][5][2] ]))  );fileclose(F);quit()',
         );
     await exec('gp -fq ./aux_files/script.gp');
     const ret = fs.readFileSync('./aux_files/cert.txt').toString();
