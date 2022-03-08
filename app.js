0be373a4e9f977aafb622c9d1dd2447f46fb72c6
Guissmo Asuncion <guissmo@gmail.com>
2022-03-08 17:09:00 +0100

    sorta covering bad cases now

--- app.js
+++ app.js
@@ -31,16 +31,30 @@ app.get('/', (req, res, next) => {
 
 app.post('/generate-pdf', (req, res, next) => {
     const filename = Math.floor(Math.random()*10000)
-    console.log(filename)
     pari.primalityCertificate(req.body.number, filename)
     const watch = fs.watch('./aux_files/', (et, fn) => {
         console.log(et)
         console.log(fn)
         if(fn === filename+'.pdf'){
             res.status(200).json({
+                isBigPrime: 1,
                 name: req.body.name,
                 number: req.body.number,
                 filename: filename
+            })
+            watch.close()
+        }
+        if(fn === `composite${ req.body.number }.txt`){
+            res.status(200).json({
+                isBigPrime: 0,
+                message: `The integer ${ req.body.number } is not prime!`
+            })
+            watch.close()
+        }
+        if(fn === `smallPrime${ req.body.number }.txt`){
+            res.status(200).json({
+                isBigPrime: 0,
+                message: `The integer ${ req.body.number } is a prime less than 18446744073709551616.`
             })
             watch.close()
         }
--- util/pari.js
+++ util/pari.js
@@ -19,42 +19,56 @@ const fsReadFileSync = util.promisify(fs.readFileSync);
 async function primalityCertificate(integer, filename) {
     await fs.writeFileSync(
         './aux_files/script.gp',
-        'F=fileopen("./aux_files/cert.txt","w");for(i=1,10^7,1+1);N=primecert('+integer+');filewrite(F, vector(#N,i, apply( x->Str(x), [i, N[i][1],N[i][2], N[i][1]+1-N[i][2], (N[i][1]+1-N[i][2])/N[i][3], N[i][4], (N[i][5][2]^2-N[i][5][1]^3-N[i][4]*N[i][5][1])%N[i][1], N[i][5][1], N[i][5][2] ]))  );fileclose(F);quit()',
+        `F=fileopen("./aux_files/cert.txt","w");N=primecert(${ integer });ret=if(type(N)=="t_INT",if(N==0, concat("C",Str(factor(${ integer },10^6))), Str(N)),vector(#N,i, apply( x->Str(x), [i, N[i][1],N[i][2], N[i][1]+1-N[i][2], (N[i][1]+1-N[i][2])/N[i][3], N[i][4], (N[i][5][2]^2-N[i][5][1]^3-N[i][4]*N[i][5][1])%N[i][1], N[i][5][1], N[i][5][2] ])));filewrite(F, ret);fileclose(F);quit()`,
         );
     await exec('gp -fq ./aux_files/script.gp');
+
     const ret = fs.readFileSync('./aux_files/cert.txt').toString();
+
+    console.log(ret[0], "~~~~")
     
-    const cert = JSON.parse(ret);
-    
-    let texCode = texHead({});
-    for(let c in cert){
-        const [i, Ni, ti, mi, qi, ai, bi, xi, yi] = cert[c];
-        texCode += texBody({
-            i: i,
-            Ni: Ni,
-            ti: ti,
-            mi: mi,
-            qi: qi,
-            ai: ai,
-            bi: bi,
-            xi: xi,
-            yi, yi
+    if(ret[0] == '[') {
+        const cert = JSON.parse(ret);
+        let texCode = texHead({});
+        for(let c in cert){
+            const [i, Ni, ti, mi, qi, ai, bi, xi, yi] = cert[c];
+            texCode += texBody({
+                i: i,
+                Ni: Ni,
+                ti: ti,
+                mi: mi,
+                qi: qi,
+                ai: ai,
+                bi: bi,
+                xi: xi,
+                yi, yi
+            })
+        }
+        texCode += texLast({
+            i: cert.length,
+            qi: cert[cert.length-1][4]
+        })
+        texCode += texFoot({});
+
+        const output = fs.createWriteStream('./aux_files/'+filename+'.pdf');
+        const pdf = latex(texCode);
+        pdf.pipe(output);
+        pdf.on('error', err => console.error(err))
+        pdf.on('finish', () => {
+            console.log('PDF generated!');
+        })
+    }else if(ret[0] == 'C'){
+        console.log("HERE!")
+        fs.rename('./aux_files/cert.txt', `./aux_files/composite${ integer }.txt`, (err) => {
+            console.log("done", err)
+        })
+    } else{
+        console.log("HERE2!")
+        fs.rename('./aux_files/cert.txt', `./aux_files/smallPrime${ integer }.txt`, (err) => {
+            console.log("done", err)
         })
     }
-    texCode += texLast({
-        i: cert.length,
-        qi: cert[cert.length-1][4]
-    })
-    texCode += texFoot({});
-
-    const output = fs.createWriteStream('./aux_files/'+filename+'.pdf');
-    const pdf = latex(texCode);
-    pdf.pipe(output);
-    pdf.on('error', err => console.error(err))
-    pdf.on('finish', () => {
-        console.log('PDF generated!');
-    })
-
+    
 }
 
 exports.primalityCertificate = primalityCertificate;
