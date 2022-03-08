0b8a7aded1a9651d8e5ef090d16d6956da47a39b
Guissmo Asuncion <guissmo@gmail.com>
2022-03-08 10:51:49 +0100

    works with post form

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require('path');
 const fs = require('fs');
 const { engine } = require('express-handlebars');
 
+const bodyParser = require("body-parser");
+
 const server = http.createServer(app);
 
 const pari = require('./util/pari');
@@ -17,34 +19,31 @@ app.set('views', 'views')
 
 app.use('/lala', express.static(path.join(__dirname, './aux_files')));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res, next) => {
-    res.render('home')
+    res.render('form')
 })
 
-app.get('/createFile', (req, res) => {
-    var fs = require("fs");
-
-    var writeStream = fs.createWriteStream("./aux_files/dynamic_file.txt");
-    writeStream.write("Hi,  Users. I am generated after the /createFile get request. ");
-    writeStream.write("Thank You.");
-    writeStream.end();
-
-    res.send('File is generated. Click <a href="./lala/dynamic_file.txt"> here </a> to see the file. Save/download the file using ctrl+s');
-});
-
-app.get('/:number', (req, res, next) => {
+app.post('/generate-pdf', (req, res, next) => {
     const filename = Math.floor(Math.random()*10000)
     console.log(filename)
-    pari.primalityCertificate("1329227995784915872903807060280345027", filename)
+    pari.primalityCertificate(req.body.number, filename)
     const watch = fs.watch('./aux_files/', (et, fn) => {
         console.log(et)
         console.log(fn)
         if(fn === filename+'.pdf'){
-            res.render('home', {filename: filename})
+            res.render('home', {
+                name: req.body.name,
+                number: req.body.number,
+                filename: filename
+            })
             watch.close()
         }
     })
-    console.log('bbb')
 })
 
 server.listen(3000)
